f4e92a7ec9e9aad58ef608e82aa69e2de96d3bd2
El Matou <elmatou@gmail.com>
2014-11-11 14:52:23 +0100

    remove LoginActivity create SettingsActivity set multiple parameters for WebActivity use builtin progressbar

--- app/src/main/java/fr/asapp/webapp/WebActivity.java
+++ app/src/main/java/fr/asapp/webapp/WebActivity.java
@@ -1,6 +1,8 @@
 package fr.asapp.webapp;
 
+import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -15,56 +17,50 @@ import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.webkit.WebChromeClient;
+import android.webkit.WebStorage;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
 
 public class WebActivity extends ActionBarActivity {
-    //    private String TheUrl = "http://asapp.github.io/appcache-basic";
-    private ProgressBar progressBar;
+    final Activity activity = this;
     private WebView myWebView;
-    private SharedPreferences sharedPref;
+    private SharedPreferences myPrefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_PROGRESS);
         setContentView(R.layout.activity_web);
+    }
 
-        progressBar = (ProgressBar) findViewById(R.id.progressBar);
-        progressBar.setMax(100);
-
-        myWebView = (WebView) findViewById(R.id.webview);
-
-        myWebView.setWebViewClient(new MyWebViewClient());
-        myWebView.setWebChromeClient(new MyWebChromeClient());
-
-        myWebView.getSettings().setJavaScriptEnabled(true);
-        myWebView.getSettings().setDomStorageEnabled(true);
-        myWebView.getSettings().setDatabaseEnabled(true);
-//        myWebView.getSettings().setDatabasePath(getApplicationContext().getCacheDir().getAbsolutePath());
-//        myWebView.getSettings().setAppCacheMaxSize(1024 * 1024 * 8);
-        myWebView.getSettings().setAppCachePath(getApplicationContext().getCacheDir().getAbsolutePath());
-        myWebView.getSettings().setAllowFileAccess(true);
-        myWebView.getSettings().setAppCacheEnabled(true);
-
-        sharedPref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-        myWebView.loadUrl(sharedPref.getString("webapp_url", ""));
+    @Override
+    protected void onStart () {
+        myPrefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        if (!myPrefs.contains("html_url")) {
+            startActivity(new Intent(WebActivity.this, SettingsActivity.class));
+        }
+        myWebView = WebViewSetup(R.id.webview, myPrefs);
+        super.onStart();
     }
 
     public class MyWebViewClient extends WebViewClient {
+//        @Override
+//        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+//            super.onPageStarted(view, url, favicon);
+//        }
 
-        @Override
-        public void onPageStarted(WebView view, String url, Bitmap favicon) {
-            progressBar.setProgress(0);
-            progressBar.setVisibility(View.VISIBLE);
-            super.onPageStarted(view, url, favicon);
-        }
+//        @Override
+//        public void onPageFinished(WebView view, String url) {
+//            super.onPageFinished(view, url);
+//        }
 
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            if (Uri.parse(url).getHost().equals(Uri.parse(sharedPref.getString("webapp_url", "")).getHost())) {
-                // This is my web site, so do not override; let my WebView load the page
+            if (Uri.parse(url).getHost().equals(Uri.parse(myPrefs.getString("html_url", "")).getHost()) || !myPrefs.getBoolean("html_external", true)) {
+                // This is my website, so do not override; let my WebView load the page
                 return false;
             }
             // Otherwise, the link is not for a page on my site, so launch another Activity that handles URLs
@@ -74,29 +70,25 @@ public class WebActivity extends ActionBarActivity {
         }
 
         @Override
-        public void onPageFinished(WebView view, String url) {
-            super.onPageFinished(view, url);
-
-            progressBar.setVisibility(View.GONE);
-            progressBar.setProgress(100);
-        }
-
-        @Override
-        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
-            super.onReceivedError(view, errorCode, description, failingUrl);
+        public void onReceivedError(final WebView webview, int errorCode, String description, String failingUrl) {
+            super.onReceivedError(webview, errorCode, description, failingUrl);
 
             new AlertDialog.Builder(WebActivity.this)
-                    .setTitle(description)
-                    .setMessage(R.string.no_app)
-                    .setPositiveButton(R.string.action_settings, new DialogInterface.OnClickListener() {
+                    .setTitle(R.string.no_app_ask)
+                    .setMessage(String.format(getResources().getString(R.string.no_app_description), description))
+                    .setPositiveButton(R.string.pref_title_system_network_settings, new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int which) {
-                            //startActivity(new Intent(WebActivity.this, SettingsActivity.class));
                             startActivity(new Intent(Settings.ACTION_WIRELESS_SETTINGS));
+                        }
+                    })
+                    .setNeutralButton(R.string.title_activity_settings, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            startActivity(new Intent(WebActivity.this, SettingsActivity.class));
                         }
                     })
                     .setNegativeButton(R.string.action_refresh, new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int which) {
-                            myWebView.reload();
+                            webview.reload();
                         }
                     })
                     .setIcon(android.R.drawable.ic_dialog_alert)
@@ -107,9 +99,13 @@ public class WebActivity extends ActionBarActivity {
     private class MyWebChromeClient extends WebChromeClient {
         @Override
         public void onProgressChanged(WebView view, int newProgress) {
-            progressBar.setProgress(newProgress);
-            super.onProgressChanged(view, newProgress);
+            activity.setProgress(newProgress * 1000);
         }
+
+//        @Override
+//        public void onGeolocationPermissionsShowPrompt (String origin, GeolocationPermissions.Callback callback) {
+//              callback.invoke(origin,true,false);
+//        }
     }
 
     @Override
@@ -126,10 +122,7 @@ public class WebActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         switch (item.getItemId()) {
             case R.id.action_sync:
-                myWebView.reload();
-                return true;
-            case R.id.action_login:
-                startActivity(new Intent(this, LoginActivity.class));
+                WebViewRefresh(myWebView);
                 return true;
             case R.id.action_settings:
                 startActivity(new Intent(this, SettingsActivity.class));
@@ -141,11 +134,52 @@ public class WebActivity extends ActionBarActivity {
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-        WebView myWebView = (WebView) findViewById(R.id.webview);
         if ((keyCode == KeyEvent.KEYCODE_BACK) && myWebView.canGoBack()) {
             myWebView.goBack();
             return true;
         }
         return super.onKeyDown(keyCode, event);
     }
+
+    public WebView WebViewSetup (Integer webview_id, SharedPreferences sharedPref) {
+
+        WebView webview = (WebView) findViewById(webview_id);
+
+        webview.setWebViewClient(new MyWebViewClient());
+        webview.setWebChromeClient(new MyWebChromeClient());
+
+        webview.getSettings().setJavaScriptEnabled(sharedPref.getBoolean("html_javascript", false));
+
+        webview.getSettings().setDomStorageEnabled(sharedPref.getBoolean("html_domstorage", false));
+
+        webview.getSettings().setDatabaseEnabled(sharedPref.getBoolean("html_localstorage", false));
+        webview.getSettings().setDatabasePath(getApplicationContext().getCacheDir().getAbsolutePath());
+
+        webview.getSettings().setAppCacheEnabled(sharedPref.getBoolean("html_appcache", false));
+        webview.getSettings().setAppCacheMaxSize(1024 * 1024 * 8);
+        webview.getSettings().setAppCachePath(getApplicationContext().getCacheDir().getAbsolutePath());
+
+//        TODO implement Geolocation,
+//        webview.getSettings().setGeolocationEnabled(sharedPref.getBoolean("html_geolocation", false));
+//        webview.getSettings().setGeolocationDatabasePath(getApplicationContext().getCacheDir().getAbsolutePath());
+//        TODO add to manifest ACCESS_COARSE_LOCATION, ACCESS_FINE_LOCATION;
+
+//        TODO Check if setAllowFileAccess(true) needed ?!
+        webview.getSettings().setAllowFileAccess(true);
+
+        webview.loadUrl(sharedPref.getString("html_url", ""));
+        return webview;
+    }
+
+    public void WebViewClear(WebView webview){
+        webview.clearCache(true);
+        //WebStorage.deleteAllData();
+        //WebStorage.deleteOrigin(String origin);
+    }
+
+    public void WebViewRefresh(WebView webview) {
+        webview.clearCache(false);
+        webview.reload();
+    }
+
 }
